80be5e6ce8b7143717120fe0e47f874f3161f29c
Marc McDougall <marc@kilobytestudios.org>
2018-04-25 11:20:35 -0400

    Added some simple styles to child theme for theming purposes.

--- wp-content/themes/crypterio-child/functions.php
+++ wp-content/themes/crypterio-child/functions.php
@@ -6,6 +6,14 @@ function crypterio_child_enqueue_parent_styles() {
 
 	wp_enqueue_style( 'crypterio-style', get_template_directory_uri() . '/style.css', array( 'bootstrap' ), CRYPTERIO_THEME_VERSION, 'all' );
 	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'crypterio-layout' ), CRYPTERIO_THEME_VERSION, 'all' );
-
-
 }
+
+// add_filter('the_title', 'remove_title_on_homepage', 10, 2);
+
+// function remove_title_on_homepage($title, $id){
+
+// 	if(is_home()){
+
+// 		return '';
+// 	}
+// }
